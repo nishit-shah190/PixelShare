bc281a3ae873b5db2e251b65c3789cbf7348aaaa
nishit-shah190 <nishitshah190@gmail.com>
2022-04-26 00:25:18 +0530

    draft

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -18,3 +18,4 @@ export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const signIn = (formData) => API.post('/users/signIn', formData);
 export const signUp = (formData) => API.post('/users/signUp', formData);
+export const comment =() => {};
